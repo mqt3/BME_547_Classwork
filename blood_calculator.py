53736a080853b74027f6d882b198955ef5cd1a35
Michael Tian <Michaeltian77@gmail.com>
2021-08-30 13:18:40 -0400

    Added ldl driver functions

--- blood_calculator.py
+++ blood_calculator.py
@@ -9,6 +9,8 @@ def interface():
 			keep_running = False
 		elif choice == 1:
 			HDL_Driver()
+		elif choice == 2:
+			LDL_Driver()
 
 	print(choice)
 	return choice
@@ -30,8 +32,29 @@ def hdl_analysis(HDL_value):
 	else:
 		return "Low"
 
-
 def hdl_output(HDL_value, HDL_answer):
 	print("The HDL value of {} is considered {}".format(HDL_value, HDL_answer))
+
+def LDL_Driver():
+	LDL_value = ldl_input()
+	LDL_character = ldl_analysis(LDL_value)
+	ldl_output(LDL_value, LDL_character)
+
+def ldl_input():
+	ldl_value = int(input("Enter LDL Value: "))
+	return ldl_value
+
+def ldl_analysis(LDL_value):
+	if LDL_value < 130:
+		return "normal"
+	elif 130 <= LDL_value <= 159:
+		return "borderline high"
+	elif 160 <= LDL_value <= 189:
+		return "high"
+	elif 190 <= LDL_value:
+		return "very high"
+
+def ldl_output(LDL_value, LDL_answer):
+	print("The LDL value of {} is considered {}".format(LDL_value, LDL_answer))
   
 interface()
