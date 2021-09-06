4178bc24ab76c4926c96ccc405ef419bb5a0ebf5
Michael Tian <Michaeltian77@gmail.com>
2021-09-06 12:43:22 -0400

    Revised dosing.py to be more modular

--- dosing.py
+++ dosing.py
@@ -6,7 +6,7 @@
 """
 
 
-def dose_amount():
+def diagnose():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -15,6 +15,9 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def weight_calc():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -24,6 +27,9 @@ def dose_amount():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+def dose_calc(diagnosis, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
@@ -34,5 +40,7 @@ def dose_amount():
 
 
 if __name__ == '__main__':
-    dose_amount()
+    diagnosis = diagnose()
+    weight = weight_calc()
+    dose_calc(diagnosis, weight)
 
